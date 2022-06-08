9b830cc4fff70cf05fcc93228afd6fbb5c32129b
Lucas Rodrigues de Araújo <99986082+lrdaraujo15@users.noreply.github.com>
2022-06-08 17:55:27 -0300

    Add files via upload

---
+++ imdbsentimentos/items.py
@@ -0,0 +1,15 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ImdbsentimentosItem(scrapy.Item):
+    # define the fields for your item here like:
+    # name = scrapy.Field()
+    author = scrapy.Field()
+    title = scrapy.Field()
+    review = scrapy.Field()
+    pass
